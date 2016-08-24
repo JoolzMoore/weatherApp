69b626c1783beefc85b643797b677f89d7461398
Julia Moore <juliamoore@Julia-Moores-MacBook-Pro-2.local>
2016-08-24 17:03:00 +1200

    got server running at last

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ var bodyParser = require ('body-parser');
 var hbs = require ('express-handlebars');
 exphbs = require ('express3-handlebars');
 
-app.engine('hbs, hbs ({extname:'hbs' defaultLayout:'layout', layoutDir __dirname + '/views/layout.hbs'})')
+app.engine('hbs', hbs ({extname:'hbs', defaultLayout:'layout', layoutDir: __dirname + '/views/layout.hbs'}));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 app.set ('views', path.join(__dirname, "views"));
-.get("/", function(req, res) {
+app.get("/", function(req, res) {
   res.send("Index Route")
 })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use('/', public, express.static('public'));
+//app.use('/', public, express.static('public'));
 app.get('/scrape', function(req, res){
 
 url = 'http://www.metvuw.com/forecast/forecast1.php?type=rain&region=nz&tim=006'
@@ -34,31 +34,10 @@ request(url, function(error, response, html){
       })
 app.use ('/', routes);
 
-    //
-    //     var $ = cheerio.load(html);
-    //
-    // var title, release, rating;
-    // var json = { title : "", release : "", rating : ""};
-    //
-    // $('.header').filter(function(){
-    //     var data = $(this);
-    //     title = data.children().first().text();
-    //     release = data.children().last().children().text();
-    //
-    //     json.title = title;
-    //     json.release = release;
-    // })
-    //
-    // $('.star-box-giga-star').filter(function(){
-    //     var data = $(this);
-    //     rating = data.text();
-    //
-    //     json.rating = rating;
-    // })
+
 }
 
-// To write to the system we will use the built in 'fs' library.
-// In this example we will pass 3 parameters to the writeFile function
+
 // Parameter 1 :  output.json - this is what the created filename will be called
 // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
 // Parameter 3 :  callback function - a callback function to let us know the status of our function
@@ -68,7 +47,7 @@ fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
     console.log('File successfully written! - Check your project directory for the output.json file');
 
 })
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+//  send out a message to the browser reminding you that this app does not have a UI.
 res.send(json)
 
 
